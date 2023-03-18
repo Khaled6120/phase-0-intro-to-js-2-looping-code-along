d355c3f98b28925bdb90a607f5c690a4f4c4c803
Khaled <khalednaes3@gmail.com>
2023-03-18 12:20:51 +0300

    Done!

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const countDown = (number) => {
+    while(number >= 0){
+        console.log(number)
+        number--
+    }
+}
+const writeCards = (names) => {
+    let count = 0
+    let manyThanks = []
+    while(count <= names.length - 1){
+        manyThanks.push(`Thank you, ${names[count]}, for the wonderful surprise gift!`)
+        count++
+    }
+    return manyThanks
+}
+
+countDown()
+writeCards(['Guadalupe', 'Ollie', 'Aki'])
